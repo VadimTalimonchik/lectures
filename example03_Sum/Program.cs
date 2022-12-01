448e1c3d33adee41b0d1df8f5a0852bef53d9d74
VadimTalimonchik <tvadmins@gmail.com>
2022-12-01 14:21:11 +0300

    Создал папку example03_Sum

---
+++ example03_Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿// сложение двух чисел
+
+// когда уже заданы числа
+// int numberA = 3;
+// int numberB = 5;
+// Console.WriteLine(numberA + numberB);
+
+// когда уже заданы числа, с введением дополнительной переменной result
+int numberA = 3;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);

0745ba41f191443d7386109c7be8e0c7dfe55fd8
VadimTalimonchik <tvadmins@gmail.com>
2022-12-01 13:48:27 +0300

    Добавил папку example02_HelooUser

---
+++ example02_HelloUser/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine(); // string - тип переменной (строка), username - переменная, Console.ReadLine("") - вводит пользователь.
+Console.Write("Привет, ");
+Console.WriteLine(username);

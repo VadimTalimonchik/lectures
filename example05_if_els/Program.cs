1a0d132b714598fa531db275128734b4adb43a46
VadimTalimonchik <tvadmins@gmail.com>
2022-12-01 16:48:59 +0300

    Добавил папку example02_if_els

---
+++ example05_if_els/Program.cs
@@ -0,0 +1,26 @@
+﻿// Console.WriteLine("Введите имя пользователя: ");
+// string username = Console.ReadLine();
+
+// if (username == "Маша") // есть проблемы, если ввести имя с маленькой буквы или любую последующую большими
+// {
+//     Console.WriteLine("Ура, это же Маша!");
+// }
+// else
+// {
+//     Console.Write("Привет, ");
+//     Console.WriteLine(username + "!");
+// }
+
+// исправил недочёт
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша") // ToLower() - все символы строки переводит в нижний регистр
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username + "!");
+}

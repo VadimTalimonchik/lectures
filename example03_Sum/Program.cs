34988b16a9e383bae21d854851b00022a9946d39
VadimTalimonchik <tvadmins@gmail.com>
2022-12-01 16:23:20 +0300

    Добавил в папку example03_Sum метод Random

--- example03_Sum/Program.cs
+++ example03_Sum/Program.cs
@@ -6,7 +6,16 @@
 // Console.WriteLine(numberA + numberB);
 
 // когда уже заданы числа, с введением дополнительной переменной result
-int numberA = 3;
-int numberB = 5;
+// int numberA = 3;
+// int numberB = 5;
+// int result = numberA + numberB;
+// Console.WriteLine(result);
+
+// Генератор случайных чисел new Random().Next(min, max)
+// числа задаются при помощи генератора случайных чисел
+int numberA = new Random().Next(1, 10); // генерируются числа от 1 2 3 ... и до 9
+Console.WriteLine(numberA); // выводится сгенерированное число в консоль
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
